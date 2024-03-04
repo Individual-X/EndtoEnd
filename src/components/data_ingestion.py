61aec2f193e62c92c1c26ed41ae5d86b87cfdb33
Sajib Hossain <sajib.hossain.limon@gmail.com>
2024-03-04 08:39:49 +0600

    Ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,8 +22,20 @@ class DataIngestion:
         try:
             df = pd.read_csv('AI\Project\EndtoEnd\Notebook\Data\stud.csv')
             logging.info('data has been read from source')
-            #making directory for the train and test data files
+            # making directory for the train and test data files
             os.makedirs(os.path.dirname(self.Ingestion_config.train_data_path), exist_ok=True)
-            df.to_csv(self.Ingestion_config.raw_data_path, Index=False, header = True)
-        except:
-            pass
+            df.to_csv(self.Ingestion_config.raw_data_path, index=False, header = True)
+            logging.info('train test split initiated.')
+            # train_test split
+            train_set, test_set = train_test_split(df, test_size = 0.20, random_state = 42)
+            train_set.to_csv(self.Ingestion_config.train_data_path, index = False, header = True)
+            logging.info('Train data created successfully')
+            test_set.to_csv(self.Ingestion_confi.test_data_path, index = False, header = True)
+            logging.info('Test data created successfully')
+            
+            return(
+                self.Ingestion_config.train_data_path
+                self.Ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
