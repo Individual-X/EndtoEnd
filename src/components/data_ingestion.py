ff0263585b46a5683ab74d3eb86e5ed36333997c
Sajib Hossain <sajib.hossain.limon@gmail.com>
2024-03-10 21:06:00 +0600

    Data ingestion driver code added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,39 +3,48 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass
 class DataIngestionConfig:
-    #Create train and test data folder
-    train_data_path: str = os.path.join('artifacts', 'train_data.csv')
-    test_data_path: str = os.path.join('artifacts', 'test_data.csv')
-    raw_data_path: str=os.path.join('artifacts', 'raw_data.csv')
+    train_data_path: str=os.path.join('E:\\Python\\AI\\Project\\EndtoEnd\\artifacts',"train.csv")
+    test_data_path: str=os.path.join('E:\\Python\\AI\\Project\\EndtoEnd\\artifacts',"test.csv")
+    raw_data_path: str=os.path.join('E:\\Python\\AI\\Project\\EndtoEnd\\artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.Ingestion_config = DataIngestionConfig()
-    
-    def Initiate_data_ingestion(self):
-        logging.info('Starting data ingestion from source')
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('AI\Project\EndtoEnd\Notebook\Data\stud.csv')
-            logging.info('data has been read from source')
-            # making directory for the train and test data files
-            os.makedirs(os.path.dirname(self.Ingestion_config.train_data_path), exist_ok=True)
-            df.to_csv(self.Ingestion_config.raw_data_path, index=False, header = True)
-            logging.info('train test split initiated.')
-            # train_test split
-            train_set, test_set = train_test_split(df, test_size = 0.20, random_state = 42)
-            train_set.to_csv(self.Ingestion_config.train_data_path, index = False, header = True)
-            logging.info('Train data created successfully')
-            test_set.to_csv(self.Ingestion_confi.test_data_path, index = False, header = True)
-            logging.info('Test data created successfully')
-            
+            df=pd.read_csv('E:\\Python\\AI\\Project\\EndtoEnd\\Notebook\\Data\\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Inmgestion of the data iss completed")
+
             return(
-                self.Ingestion_config.train_data_path
-                self.Ingestion_config.test_data_path
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
             )
         except Exception as e:
             raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+
